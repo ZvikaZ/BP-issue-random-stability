bfc5fb86a75ff19bedfdd738ba8a2d232e8bd841
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-06-01 10:34:10 +0300

    demonstrates random stability issue

--- src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
@@ -4,6 +4,7 @@ import il.ac.bgu.cs.bp.bpjs.execution.BProgramRunner;
 import il.ac.bgu.cs.bp.bpjs.execution.listeners.PrintBProgramRunnerListener;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.ResourceBProgram;
+import il.ac.bgu.cs.bp.bpjs.model.eventselection.PrioritizedBSyncEventSelectionStrategy;
 
 /**
  * Simple class running a BPjs program that selects "hello world" events.
@@ -13,8 +14,13 @@ public class HelloWorld {
     
     public static void main(String[] args) throws InterruptedException {
         // This will load the program file  <Project>/src/main/resources/HelloBPjsWorld.js
-        final BProgram bprog = new ResourceBProgram("HelloBPjsWorld.js");
-        
+        final BProgram bprog = new ResourceBProgram("FourInARow.js");
+
+        var prio = new PrioritizedBSyncEventSelectionStrategy(1);
+        prio.setDefaultPriority(0);
+        bprog.setEventSelectionStrategy(prio);
+
+
         BProgramRunner rnr = new BProgramRunner(bprog);
 
         // Print program events to the console
